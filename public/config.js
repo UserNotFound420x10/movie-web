873f475173836223171af90fe2c93963fc22ce4b
UserNotFound420x10 <150506082+UserNotFound420x10@users.noreply.github.com>
2023-11-16 16:45:07 +0100

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 window.__CONFIG__ = {
   // url must NOT end with a slash
-  VITE_CORS_PROXY_URL: "",
-  VITE_TMDB_READ_API_KEY: ""
+  VITE_CORS_PROXY_URL: "https://worker-neg.weathered-meadow-bce9.workers.dev/",
+  VITE_TMDB_READ_API_KEY: "0909ad60f1c6d5757a2b64571d71f37d"
 };
